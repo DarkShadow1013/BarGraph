44f1b0b16feeff4e7548665e44279379603a65ea
DarkShadow1013 <64131675+DarkShadow1013@users.noreply.github.com>
2021-07-11 15:35:52 +0800

    Update m.js

--- m.js
+++ m.js
@@ -12,7 +12,10 @@ c.stroke()
 
 //document.getElementById("max").value
 
+var y = [0, 0, 0, 0, 0]
+create()
 function create() {
+    requestAnimationFrame(create)
     c.clearRect(0,0,2000,2000)
     c.beginPath()
     c.beginPath()
@@ -25,35 +28,16 @@ function create() {
     c.fillText(document.getElementById("max").value, 20, 20)
     c.font = "20px arial"
     c.fillText(0, 20, 690)
-    //
-    c.fillStyle = document.getElementById("c1").value
-    c.fillRect(150,680,90, (-1 * document.getElementById("1").value) /  document.getElementById("max").value * 684)
-    c.rect
-    c.font = "20px arial"
-    c.fillText(document.getElementById("n1").value, 150, 710)
-    //
-    c.fillStyle = document.getElementById("c2").value
-    c.fillRect(350,680,90,(-1 * document.getElementById("2").value) /  document.getElementById("max").value * 684)
-    c.rect
-    c.font = "20px arial"
-    c.fillText(document.getElementById("n2").value, 350, 710)
-    //
-    c.fillStyle = document.getElementById("c3").value
-    c.fillRect(550,680,90,(-1 * document.getElementById("3").value) /  document.getElementById("max").value * 684)
-    c.rect
-    c.font = "20px arial"
-    c.fillText(document.getElementById("n2").value, 550, 710)
-    //
-    c.fillStyle = document.getElementById("c4").value
-    c.fillRect(750,680,90,(-1 * document.getElementById("4").value) /  document.getElementById("max").value * 684)
-    c.rect
-    c.font = "20px arial"
-    c.fillText(document.getElementById("n4").value, 750, 710)
-    //
-    c.fillStyle = document.getElementById("c5").value
-    c.fillRect(950,680,90,(-1 * document.getElementById("5").value) /  document.getElementById("max").value * 684)
-    c.rect
-    c.font = "20px arial"
-    c.fillText(document.getElementById("n5").value, 950, 710)
     
+    for (var i = 1; i < 6; i ++) {
+        y[i - 1] = lerp(y[i - 1], (-1 * document.getElementById(i.toString()).value), 0.01)
+        c.fillStyle = document.getElementById("c" + i.toString()).value
+        c.fillRect(150 + 200 * i - 200,680,90,  y[i - 1] /  document.getElementById("max").value * 684)
+        c.fillText(document.getElementById("n" + i.toString()).value, 150 + 200 * i - 200, 710)
+    }
+    
+}
+
+function lerp(a, b, f) {
+    return (a * (1 - f)) + (b * f);
 }
